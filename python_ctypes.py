e579d7239be5039330e8c5f0e84a7c034dff15a6
Rakshith Reddy Bollu <60643500+rakshithbollu@users.noreply.github.com>
2022-11-14 00:21:48 -0800

    Create python_ctypes.py

---
+++ python_ctypes.py
@@ -0,0 +1,61 @@
+# import ctypes python and c/c++ binding library.
+import ctypes
+import time
+import numpy as np
+
+# generate .so file from the .c file and invike the .so file in the .py file
+librefObject = ctypes.CDLL('/home/rakshith/maxprofit.so')
+
+# using numpy randint function generate a list of random integers which is used as the input for the algorithm.
+prices= np.random.randint(100,1000,500)
+
+sample_size = len(prices)
+
+def toArray(list):
+        pythonctypesarray=len(list) * ctypes.c_int
+        integerarray=pythonctypesarray()
+        current=0
+        for element in list:
+                integerarray[int(current)]=int(element)
+                current=current+1
+        return integerarray
+
+prices = toArray(prices)
+
+# Invoke the maxprofit function signature.
+maxprofit = librefObject.maxprofit
+
+# define the input arguments syntax
+maxprofit.argtypes = [ctypes.c_void_p]
+# define the return type of the function
+maxprofit.restype= ctypes.c_int
+
+# Get the time stamp before the function execution.
+start_time = time.time()
+
+# function call
+profit = maxprofit(prices,sample_size)
+
+print("the max profit calculated is " , profit)
+
+# Get the time stamp after the function execution.
+print("--- time taken in Python ctypes is %s seconds---" % (time.time() - start_time))
+
+
+# defining the same alogorithm in pure python
+def maxProfit(prices):
+    max_profit = 0
+    for i in range(len(prices) - 1):
+        for j in range(i + 1, len(prices)):
+            profit = prices[j] - prices[i]
+            if profit > max_profit:
+                max_profit = profit
+        return max_profit
+
+# Get the time stamp before the function execution.
+start_time_python = time.time()
+
+# function call
+maxProfit(prices)
+# Get the time stamp after the function execution.
+print("--- time taken in pure Python is %s seconds ---" % (time.time() - start_time_python))
